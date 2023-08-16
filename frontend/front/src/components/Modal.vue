2eef245059009e68f0724cd075e723f4f7029dfc
juan vertel <juandavidvertelh@gmail.com>
2023-08-16 11:24:50 -0500

    desarrollo de interfaz resulelto modal, falta daots via prop

--- frontend/front/src/components/Modal.vue
+++ frontend/front/src/components/Modal.vue
@@ -1,22 +1,138 @@
+
 <!-- componente de modal accesible desde dualquier -->
 <template>
-  <h1> componente de modal {{ title }} </h1>
-  <button @click="ocultar" v-if="!mostrarmodal"> activar</button>
+  <transition>
+    <div class="modal2-overlay" v-if="title">
+    </div>
+  </transition>
+  <transition>
+
+
+    <div class="modal2">
+      <h4>{{ title }} vacunas </h4>
+      <hr>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-4">
+            <input class="form-control form-control-sm" type="text" placeholder="Codigo CUPS" v-model="cups" />
+          </div>
+          <div class="col-12 col-sm-4">
+            <input class="form-control form-control-sm" type="text" placeholder="Nombre Biologico" v-model="nombre" />
+          </div>
+          <div class="col-12 col-sm-4">
+            <select class="form-select" aria-label="Default select example" v-model="aplicacion">
+              <option value="">Aplicacion</option>
+              <option value="I">Inyectable</option>
+              <option value="O">Oral</option>
+            </select>
+          </div>
+          <div class="col-12 col-sm-4">
+            <select class="form-select" aria-label="Default select example" v-model="diluyente">
+              <option value="">Diluyente</option>
+              <option value="1">SI</option>
+              <option value="0">NO</option>
+            </select>
+          </div>
+          <div class="col-12 col-sm-4">
+            <select class="form-select" aria-label="Default select example" v-model="jeringa">
+              <option value="">Calibre Jeringa</option>
+              <option value="22">22</option>
+              <option value="23">23</option>
+              <option value="25">25</option>
+              <option value="26">26</option>
+            </select>
+          </div>
+          <div class="col-12 col-sm-4">
+            <select class="form-select" aria-label="Default select example" v-model="sexo">
+              <option value="">Sexo</option>
+              <option value="A">Ambos</option>
+              <option value="M">Masculino</option>
+              <option value="F">Femenino</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <hr>
+      <button class="btn_c" @click="ocultar"> Cerrar</button>
+      <button class="btn_e" @click="guardar"> Editar</button>
+
+
+    </div>
+
+  </transition>
 </template>
 
 <script>
 export default {
-  name: 'modalDinamico',
+
+  /*  lo que viene del pádre hacia el hijo*/
   props: ['title'],
+
+  data() {
+
+    return {
+      estadomodal: false,
+      cups: "",
+      nombre: "",
+      aplicacion: "",
+      diluyente: "",
+      jeringa: "",
+      sexo: "",
+    }
+  },
+
   methods: {
     ocultar() {
-      this.mostrarmodal = !this.mostrarmodal,
-        this.$emit('datoshijo', this.mostrarmodal);
-      console.log(this.mostrarmodal)
+      /* lo que viaja al padre desde el hijo */
+      this.$emit('rta_hijo', this.estadomodal);
+      console.log(this.estadomodal)
+    },
+
+    guardar() {
+      /* lo que viaja al padre desde el hijo */
+      /*   this.$emit('rta_hijo', this.estadomodal); */
+      console.log(this.estadomodal)
     }
   }
 }
 
 </script>
 
-<style></style>
+<style scoped>
+.modal2-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 98;
+  background-color: rgb(19, 19, 19);
+  opacity: 0.5;
+}
+
+.modal2 {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #ffffff;
+  padding: 20px;
+  border-radius: 15px;
+  z-index: 105;
+
+}
+
+.btn_c {
+  border-radius: 5px !important;
+  background-color: rgb(245, 25, 25) !important;
+  color: #ffffff;
+  border: none;
+}
+
+.btn_e {
+  border-radius: 5px !important;
+  background-color: rgb(6, 212, 88) !important;
+  color: #ffffff;
+  border: none;
+}
+</style>
--- frontend/front/src/views/VacunasView.vue
+++ frontend/front/src/views/VacunasView.vue
@@ -7,9 +7,7 @@
       <h6>Listado de Vacunas del sistema</h6>
     </div>
     <div class="col-2">
-      <button class="btn btn-success btn-sm" data-bs-toggle="modal" data-bs-target="#ModalVacunas">
-        +
-      </button>
+
     </div>
   </div>
 
@@ -29,14 +27,14 @@
       </thead>
       <tbody>
         <tr v-for="vacuna in vacunas">
-          <td> {{ vacuna.cups }} </td>
+          <td>{{ vacuna.cups }} </td>
           <td>{{ vacuna.nombre }}</td>
           <td>{{ vacuna.aplicacion }}</td>
           <td>{{ vacuna.diluyente }}</td>
           <td>{{ vacuna.jeringa }}</td>
           <td>{{ vacuna.sexo }}</td>
 
-          <td> <button @click="mostrar" v-if="!mostrarmodal"> activar</button>
+          <td> <button class="btnm" @click="mostrar" v-if="!mostrarmodal"> activar</button>
           </td>
         </tr>
 
@@ -45,80 +43,9 @@
   </div>
 
 
-  <modal title="titulodesdepadre" v-if="mostrarmodal" />
+  <modal title="titulodesdepadre" v-if="mostrarmodal" @rta_hijo="this.mostrarmodal = $event" />
 
-  <div class="modal fade" id="ModalVacunas" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h1 class="modal-title fs-5" id="exampleModalLabel">Crear Vacuna</h1>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          <!--  ... -->
-
-          <div class="container">
-            <div class="row">
-              <div class="col-6 col-sm-4">
-                <input class="form-control form-control-sm" type="text" placeholder="Codigo CUPS" v-model="cups" />
-              </div>
-              <div class="col-6 col-sm-4">
-                <input class="form-control form-control-sm" type="text" placeholder="Nombre Biologico" v-model="nombre" />
-              </div>
-
-
-              <div class="col-6 col-sm-4">
-                <select class="form-select" aria-label="Default select example" v-model="aplicacion">
-                  <option value="">Aplicacion</option>
-                  <option value="I">Inyectable</option>
-                  <option value="O">Oral</option>
-                </select>
-              </div>
-              <div class="col-6 col-sm-4">
-                <select class="form-select" aria-label="Default select example" v-model="diluyente">
-                  <option value="">Diluyente</option>
-                  <option value="1">SI</option>
-                  <option value="0">NO</option>
-                </select>
-              </div>
-              <div class="col-6 col-sm-4">
-                <select class="form-select" aria-label="Default select example" v-model="jeringa">
-                  <option value="">Calibre Jeringa</option>
-                  <option value="22">22</option>
-                  <option value="23">23</option>
-                  <option value="25">25</option>
-                  <option value="26">26</option>
-                </select>
-              </div>
-              <div class="col-6 col-sm-4">
-                <select class="form-select" aria-label="Default select example" v-model="sexo">
-                  <option value="">Sexo</option>
-                  <option value="A">Ambos</option>
-                  <option value="M">Masculino</option>
-                  <option value="F">Femenino</option>
-                </select>
-              </div>
-            </div>
-          </div>
-     
-
-
-
-          <!--  -->
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-            Cerrar
-          </button>
-          <button type="button" class="btn btn-primary">Guardar</button>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div    @datoshijo = "this.mostrarmodal = $event">
-
-  </div>
-
+  <!--    -->
 </template>
 
 <script>
@@ -126,16 +53,24 @@ import Modal from "./../components/Modal.vue";
 
 export default {
 
+
+
   components: {
     Modal
   },
 
 
+
   data() {
-   
+
     return {
       mostrarmodal: false,
-
+      aplicacion: "",
+      diluyente: "",
+      jeringa: "",
+      sexo: "",
+      cups: "",
+      nombre: "",
       vacunas: [
         {
           aplicacion: "I",
@@ -154,34 +89,34 @@ export default {
           nombre: "pfizer"
         }
       ],
-   
-        aplicacion: "",
-        diluyente: "",
-        jeringa: "",
-        sexo: "",
-        cups: "",
-        nombre: ""
-      
+
+
 
     }
 
 
   },
+
+
   methods: {
     mostrar() {
       this.mostrarmodal = !this.mostrarmodal,
-      console.log(this.mostrarmodal)
+        console.log(this.mostrarmodal)
     }
-
-
-  
+  }
 }
 
 </script>
 
-<style>
-th {
+<style scoped>
+.th {
   font-weight: bold;
 }
+
+.btnm {
+  border-radius: 5px !important;
+  background-color: aquamarine !important;
+  border: 0px;
+}
 </style>
-https://www.youtube.com/watch?v=QqjO9LulR3M
+/* https://www.youtube.com/watch?v=QqjO9LulR3M */
